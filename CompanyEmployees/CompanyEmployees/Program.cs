a70f2b8287c5e8236888fb680f5f25ae24b31e46
Miguel Palacio <miguel.moyano@soy.sena.edu.co>
2023-08-14 23:06:17 -0500

    Commit Inicial: Configuraciones ServiceExtensions y Program

---
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -0,0 +1,50 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureCors(); 
+builder.Services.ConfigureIISIntegration();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+        app.UseDeveloperExceptionPage();
+else 
+        app.UseHsts();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles(); 
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions 
+{
+    ForwardedHeaders = ForwardedHeaders.All 
+}); 
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.Run(async context => 
+{
+    await context.Response.WriteAsync("Hello from the middleware component."); 
+});
+
+app.MapControllers();
+
+app.Run();
