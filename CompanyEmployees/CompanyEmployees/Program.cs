b73aea8d22b50fe03fdeed14b380c6520189a471
Miguel Palacio <miguel.moyano@soy.sena.edu.co>
2023-08-15 08:52:54 -0500

    feat(Backend): Añadiendo Use Method

--- CompanyEmployees/CompanyEmployees/Program.cs
+++ CompanyEmployees/CompanyEmployees/Program.cs
@@ -40,9 +40,27 @@ app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
-app.Run(async context => 
-{
-    await context.Response.WriteAsync("Hello from the middleware component."); 
+//app.Run(async context => 
+//{
+//    await context.Response.WriteAsync("Hello from the middleware component."); 
+//});
+
+app.Use(async (context, next) => {
+
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+
+    await next.Invoke();
+
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+
+});
+
+app.Run(async context => {
+
+    Console.WriteLine($"Writing the response to the client in the Run method");
+
+    await context.Response.WriteAsync("Hello from the middleware component.");
+
 });
 
 app.MapControllers();
